bd9079961f1c3b26b887278f8ccd734996682a2d
Анастасия Кольцова <kolcova1910@mail.ru>
2022-09-04 04:19:15 +0300

    Commit

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import include, path 
+from django.urls import include, path
 
-urlpatterns = [       
+urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
 ]
